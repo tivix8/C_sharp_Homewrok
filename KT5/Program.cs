86f7c802131014af1c0e74b317ff1a31e61e785c
tivix8 <tarubarov08@gmail.com>
2025-10-16 22:46:27 +1000

    KT5

---
+++ KT5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT5 {
+    internal class Program {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Movie
+    {
+        public string Name { get; set; };
+        public string Duration { get; set; };
+        public static List<Movie> Movies { get; set; } = new List<Movie>();
+    }
+    public class Seat
+    {
+        public int Number { get; set; }
+        public bool IsReserve { get; private set; }
+
+        public void Reserve()
+        {
+            if (!IsOccupied)
+            {
+                IsReserve = true;
+                Console.WriteLine($"Место {Number} забранировано")
+            }
+            else 
+            { 
+                Console.WriteLine($"Место {Number} занято, выберите другое место"
+            }
+
+        }
+
+
+    }
+    public class BookingService
+    {
+        public List<Movie> GetSeat()
+        {
+           return List<Movie>
+        }
+    }
+    public class CinemaHall
+    {
+
+    }
+
+
+
+}
