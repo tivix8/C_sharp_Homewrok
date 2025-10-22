fd5c089b85042f641a365ca567c078bb73ddef7c
tivix8 <tarubarov08@gmail.com>
2025-10-22 20:38:01 +1000

    KT1

---
+++ KT1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT1 {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выберите операцию (+, -, *, /):");
+            char chr = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine();
+            Calculate(a, b, chr);
+            Console.WriteLine();
+            Console.WriteLine("Анализ первого числа:");
+            Analysis(a);
+            Console.WriteLine("Анализ второго числа:");
+            Analysis(b);
+
+
+
+        }
+        public static void Calculate(double a, double b, char chr)
+        {
+
+            switch (chr)
+            {
+                case '+':
+                    Console.WriteLine($"Резултат {a + b}"); break;
+                case '-':
+                    Console.WriteLine($"Резултат {a - b}"); break;
+                case '*':
+                    Console.WriteLine($"Резултат {a * b}"); break;
+                case '/':
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Делить на но 0 нельзя");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Резултат {a / b}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Операция не найдена");
+                    break;
+                            
+                    
+            }
+            
+        }
+        
+        public static void Analysis(double num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("Ноль");
+                return;
+            }
+
+            string parity = (num % 2 == 0) ? "четное" : "нечетное";
+            string numbertype = (num > 0) ? "положительное" : "отрицательное";
+
+            Console.WriteLine($"{num} - {parity}, {numbertype} число");
+        }
+
+
+    }
+
+        
+}
---
+++ KT1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("KT1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KT1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("57bf8f00-f9d4-49b6-86bc-7fff1025a2d2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
